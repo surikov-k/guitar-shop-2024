cda041ebeb47ab5f2dd35a5ebd852c26f89afa0e
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-06 14:36:26 +0300

    feat(shop): Add a Shop Item module of the Shop service

--- apps/shop/src/app/app.module.ts
+++ apps/shop/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ShopItemModule } from './shop-item/shop-item.module';
 
 @Module({
-  imports: [],
+  imports: [ShopItemModule],
   controllers: [],
   providers: [],
 })
