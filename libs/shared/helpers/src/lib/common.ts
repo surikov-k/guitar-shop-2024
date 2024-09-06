17049bc239e4319b92170908796a7d2cab8008d9
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-06 15:45:56 +0300

    feat(project): Add shared helpers

---
+++ libs/shared/helpers/src/lib/common.ts
@@ -0,0 +1,26 @@
+import { ClassTransformOptions, plainToInstance } from 'class-transformer';
+
+type PlainObject = Record<string, unknown>;
+
+export function prepareDto<T, V extends PlainObject>(
+  DtoClass: new () => T,
+  plainObject: V,
+  options?: ClassTransformOptions,
+): T;
+
+export function prepareDto<T, V extends PlainObject[]>(
+  DtoClass: new () => T,
+  plainObject: V,
+  options?: ClassTransformOptions,
+): T[];
+
+export function prepareDto<T, V extends PlainObject>(
+  DtoClass: new () => T,
+  plainObject: V,
+  options?: ClassTransformOptions,
+): T | T[] {
+  return plainToInstance(DtoClass, plainObject, {
+    excludeExtraneousValues: true,
+    ...options,
+  });
+}
