51b1e6256db207ef3af9b132ce1c71f83ff9cc15
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-07 17:00:35 +0300

    feat(api-gate): Add a controller and service to the auth module

--- apps/api-gate/src/app/auth/auth.controller.ts
+++ apps/api-gate/src/app/auth/auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { LoginDto, RegisterDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  public async register(@Body() dto: RegisterDto) {
+    try {
+
+      return await this.authService.register(dto);
+    } catch ({ message, status }) {
+      throw new HttpException(message, status);
+    }
+  }
+
+
+  @Post('login')
+  public async login(@Body() dto: LoginDto) {
+    try {
+      return await this.authService.login(dto);
+    } catch ({ message, status }) {
+      throw new HttpException(message, status);
+    }
+  }
+}
--- apps/api-gate/src/app/auth/auth.service.ts
+++ apps/api-gate/src/app/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { Inject, Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+
+import { UserEvent } from '@guitar-shop-2024/types';
+import { LoginDto, RegisterDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject('USER_SERVICE') private readonly userService: ClientProxy
+  ) {}
+
+  public register(dto: RegisterDto) {
+    return firstValueFrom(
+      this.userService.send({ cmd: UserEvent.Register }, { dto })
+    );
+  }
+
+  public login(dto: LoginDto) {
+    return firstValueFrom(
+      this.userService.send({ cmd: UserEvent.Login }, { dto })
+    );
+  }
+}
