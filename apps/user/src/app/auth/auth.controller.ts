ad3a23faa7eedc363f796ac6deb8a9dfb0312250
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-07 16:31:57 +0300

    feat(user): Add an Auth controller

--- apps/user/src/app/auth/auth.controller.ts
+++ apps/user/src/app/auth/auth.controller.ts
@@ -1,4 +1,40 @@
+import { LoginInterface, RegisterInterface, UserEvent } from '@guitar-shop-2024/types';
 import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @MessagePattern({ cmd: UserEvent.Register })
+  public async register(@Payload() { dto }: { dto: RegisterInterface }) {
+    return await this.authService.register(dto);
+  }
+
+  @MessagePattern({ cmd: UserEvent.Login })
+  public async login(@Payload() { dto }: { dto: LoginInterface }) {
+    return this.authService.login(dto);
+  }
+
+  @MessagePattern({ cmd: UserEvent.Verify })
+  public async verify(@Payload() { dto }: { dto: LoginInterface }) {
+    return this.authService.verify(dto);
+  }
+
+  @MessagePattern({ cmd: UserEvent.CheckEmail })
+  checkEmail(
+    @Payload()
+      { email }: { email: string }
+  ) {
+    return this.authService.checkEmail(email);
+  }
+
+  @MessagePattern({ cmd: UserEvent.GetUser })
+  getUser(
+    @Payload()
+      { userId }: { userId: string }
+  ) {
+    return this.authService.getUser(userId);
+  }
+}
