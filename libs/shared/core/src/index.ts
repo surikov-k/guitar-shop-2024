e5ba551fe1094bfa25b445ef0cc50112b11c2178
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-07 13:19:35 +0300

    feat(project): Add an abstract base mongo repository

--- libs/shared/core/src/index.ts
+++ libs/shared/core/src/index.ts
@@ -1,2 +1,3 @@
-export * from './lib/repository/repository.interface';
+export * from './lib/repository/base-mongo.repository';
 export * from './lib/repository/entity.interface';
+export * from './lib/repository/repository.interface';
