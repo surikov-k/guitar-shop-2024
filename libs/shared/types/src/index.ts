0d05d5ef9b052f873629de58adff1e991ed24bae
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-07 15:08:16 +0300

    feat(project): Add a users event enum

--- libs/shared/types/src/index.ts
+++ libs/shared/types/src/index.ts
@@ -1,7 +1,7 @@
 export * from './lib/auth-user.interface';
 export * from './lib/file.interface';
-export * from './lib/guitar.interface';
 export * from './lib/guitar-type.enum';
+export * from './lib/guitar.interface';
 export * from './lib/string-number.type';
 export * from './lib/user.interface';
-
+export * from './lib/users-event.enum';
---
+++ libs/shared/types/src/lib/users-event.enum.ts
@@ -0,0 +1,10 @@
+export enum UserEvent {
+  CheckAuth = 'CheckAuth',
+  CheckEmail = 'CheckEmail',
+  GetAll = 'GetAll',
+  GetUser = 'GetUser',
+  Login = 'Login',
+  Logout = 'Logout',
+  Register = 'Register',
+  Verify = 'Verify',
+}
