7f3cdda33ebc0ffde4a6ca55b8004a60abd4c507
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-07 13:25:05 +0300

    feat(project): Add exports to the type library

---
+++ libs/shared/types/src/index.ts
@@ -0,0 +1,7 @@
+export * from './lib/auth-user.interface';
+export * from './lib/file.interface';
+export * from './lib/guitar.interface';
+export * from './lib/guitar-type.enum';
+export * from './lib/string-number.type';
+export * from './lib/user.interface';
+
