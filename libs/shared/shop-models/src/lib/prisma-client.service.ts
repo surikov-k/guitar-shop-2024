48f74942426e789c78a721401c1fbea7eaa7d4ce
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-08 22:37:00 +0300

    feat(shop): Add a Prisma module

---
+++ libs/shared/shop-models/src/lib/prisma-client.service.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+
+@Injectable()
+export class PrismaClientService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
