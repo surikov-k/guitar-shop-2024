9ed46255f79da4fd0a7146599e4c894a7453e298
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-08 11:05:50 +0300

    feat(api-gate): Add a User RDO to the Auth module

--- apps/api-gate/src/app/auth/auth.controller.ts
+++ apps/api-gate/src/app/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Post, UseGuards } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from './dto';
@@ -6,6 +6,13 @@ import { LoginDto, RegisterDto } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  @UseGuards(AccessTokenGuard)
+  public async checkAuth(@CurrentUserRole() role: UserRole) {
+    return role;
+  }
+
 
   @Post('register')
   public async register(@Body() dto: RegisterDto) {
---
+++ apps/api-gate/src/app/auth/rdo/user.rdo.ts
@@ -0,0 +1,12 @@
+import { Expose } from 'class-transformer';
+
+export class UserRdo {
+  @Expose()
+  public id: string;
+
+  @Expose()
+  public email: string;
+
+  @Expose()
+  public name: string;
+}
