f3fb564fa59171944aa9ebaa375b2e55b1d8a3a6
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-08 22:39:26 +0300

    feat(project): Add a base postgres repository

--- libs/shared/core/src/index.ts
+++ libs/shared/core/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/repository/base-mongo.repository';
+export * from './lib/repository/base-postgres.repository';
 export * from './lib/repository/entity.interface';
 export * from './lib/repository/repository.interface';
