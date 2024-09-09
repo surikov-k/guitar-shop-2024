d7c97be0c60ed77115b1ec12dc6cdcf7547a573f
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-09-09 16:40:45 +0300

    feat(shop): Add a Shop Item service

--- apps/shop/src/app/shop-item/shop-item.service.ts
+++ apps/shop/src/app/shop-item/shop-item.service.ts
@@ -1,4 +1,42 @@
+import { Guitar, ShopItemQueryInterface } from '@guitar-shop-2024/types';
 import { Injectable } from '@nestjs/common';
+import { ShopItemEntity } from './shop-item.entity';
+import { ShopItemRepository } from './shop-item.repository';
 
 @Injectable()
-export class ShopItemService {}
+export class ShopItemService {
+  constructor(
+    private readonly shopItemRepository: ShopItemRepository
+  ) {
+  }
+
+  async create(dto: Guitar): Promise<Guitar> {
+    const entity = ShopItemEntity.fromObject(dto);
+    return this.shopItemRepository.create(entity);
+  }
+
+  async delete(id: string) {
+    await this.shopItemRepository.destroy(id);
+  }
+
+  async get(id: string): Promise<Guitar> {
+    return this.shopItemRepository.findById(id);
+  }
+
+  async getAll(query: ShopItemQueryInterface): Promise<Guitar[]> {
+    return this.shopItemRepository.find(query);
+  }
+
+
+  async update(id: string, dto: Guitar): Promise<Guitar> {
+    const item = await this.shopItemRepository.findById(id);
+
+    const entity = ShopItemEntity
+      .fromObject({
+        ...item,
+        ...dto
+      });
+
+    return this.shopItemRepository.update(id, entity);
+  }
+}
